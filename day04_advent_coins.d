8f4185ddd611ff87c36c1a759e1feb8414e94d5e
Peter Tseng <pht24@cornell.edu>
2017-03-15 03:08:05 +0000

    day 04: Use a single buffer and sprintf into it
    Down from 3.9 seconds to 3.2 seconds now.

--- day04_advent_coins.d
+++ day04_advent_coins.d
@@ -3,21 +3,29 @@ name "aoc04"
 +/
 
 void main(string[] args) {
+  import core.stdc.stdio : sprintf;
   import std.algorithm : all;
-  import std.conv : to;
   import std.digest.md : md5Of;
   import std.file : readText;
   import std.stdio : writefln;
 
   immutable string input = args.length <= 1 ? readText("/dev/stdin") : args[1];
+  char[] buf = input.dup;
 
   int zeroes = 1;
   int n = 0;
+  size_t len = input.length + 1;
+  int nextTen = 10;
+  // Do I need this + 1 for the extra null byte?
+  buf.length = len + 1;
 
   while (zeroes <= 6) {
     int fullBytes = zeroes >> 1;
     bool halfByte = (zeroes & 1) == 1;
-    auto md5 = md5Of(input ~ to!string(n));
+
+    sprintf(buf.ptr + input.length, "%d", n);
+    auto md5 = md5Of(buf[0 .. len]);
+
     if (all!"a == 0"(md5[0..fullBytes]) && (!halfByte || (md5[fullBytes] & ~15) == 0)) {
       if (zeroes >= 5) {
         writefln("%d", n);
@@ -25,6 +33,11 @@ void main(string[] args) {
       ++zeroes;
     } else {
       ++n;
+      if (n == nextTen) {
+        nextTen *= 10;
+        ++len;
+        ++buf.length;
+      }
     }
   }
 }
