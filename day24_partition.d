eb3e01b8a5b9a4252a1ed75945ee6c07898af2cd
Peter Tseng <pht24@cornell.edu>
2017-07-19 07:17:41 +0000

    add day 24

---
+++ day24_partition.d
@@ -0,0 +1,65 @@
+/+ dub.sdl:
+name "aoc24"
+dependency "adventofcode" path="."
++/
+
+pure @safe nothrow size_t minNeeded(const ulong[] weights, ulong target) {
+  import std.algorithm : sort;
+
+  ulong weightSoFar = 0;
+  size_t n = 0;
+
+  foreach (weight; weights.dup.sort!("a > b")) {
+    ++n;
+    weightSoFar += weight;
+    if (weightSoFar >= target) {
+      return n;
+    }
+  }
+
+  return weights.length;
+}
+
+// https://rosettacode.org/wiki/Combinations#More_Functional_Recursive_Version
+// I picked this one because it was short.
+pure @safe nothrow immutable(ulong)[][] combinations(const ulong[] s, size_t m) {
+  import std.algorithm : map;
+  import std.array : array;
+
+  if (!m) return [[]];
+  if (s.length == 0) return [];
+  return s[1 .. $].combinations(m - 1).map!(x => s[0] ~ x).array ~ s[1 .. $].combinations(m);
+}
+
+pure @safe immutable(ulong)[] partition(const ulong[] weights, uint numGroups) {
+  import std.algorithm : sum;
+
+  ulong total = weights.sum;
+  ulong eachGroup = total / numGroups;
+
+  for (size_t i = minNeeded(weights, eachGroup); i <= weights.length / numGroups; ++i) {
+    import std.algorithm : filter, minElement, reduce;
+    import std.array : array;
+
+    // Incorrect: Does not check for partitionability on the winning set.
+    auto winningCombos = array(weights.combinations(i).filter!(a => a.sum == eachGroup));
+    if (winningCombos.length != 0) {
+      return winningCombos.minElement!(c => c.reduce!("a * b"));
+    }
+  }
+
+  return weights.dup;
+}
+
+void main(string[] args) {
+  import std.algorithm : reduce;
+  import std.file : slurp;
+  import std.stdio : writeln;
+
+  const weights = slurp!(ulong)(args.length <= 1 ? "/dev/stdin" : args[1], "%d");
+
+  foreach (n; [3, 4]) {
+    const best = partition(weights, n);
+    writeln(best.reduce!("a * b"));
+  }
+}
